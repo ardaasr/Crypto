6080027edcb8020dcba342b425d53eee96ac526d
ardasarı <153986769+ardaasr@users.noreply.github.com>
2025-04-03 19:35:48 +0300

    Update ApplicationUser.cs

--- Crypto/Identity/ApplicationUser.cs
+++ Crypto/Identity/ApplicationUser.cs
@@ -5,7 +5,5 @@ namespace Crypto.Identity
     public class ApplicationUser:IdentityUser
     {
         public string FullName { get; set; }
-
-        public override string? UserName { get; set; }
     }
 }
